2248a8a7d99bc2b0596a0ab4f6fa50535128bab3
David Antonio Gracia Escobar <davidgaresc87@hotmail.com>
2015-09-21 19:54:49 -0500

    Primera version para conocer si un numero es primo en Swift

--- Es Primo/ViewController.swift
+++ Es Primo/ViewController.swift
@@ -9,6 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numero: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func isPrime(sender: AnyObject) {
+        
+        var number = numero.text.toInt()
+        var isPrime = true
+        
+        if number == nil || number <= 0 {
+            result.text = "Debes de introducitr un Numero positivo"
+        } else {
+        if number == 1 || number == 2 {
+            isPrime = false
+        } else {
+            for var i = 2; i < number; i++ {
+                if number! % i == 0 {
+                    isPrime = false
+                }
+            }
+        }
+        
+        if isPrime == false {
+            result.text = " \(number!) no es Primo"
+            result.hidden = false
+        } else {
+            result.text = "\(number!) es un numero Primo"
+            result.hidden = false
+        }
+        
+        }
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
